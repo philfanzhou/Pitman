5c6ef179ca24e5255111a59f66d06d20fb1e4ab5
philfanzhou <philfanzhou@hotmail.com>
2015-09-29 14:56:33 +0800

    调整项目结构

---
+++ 2_Application/Pitman.Application.MarketData/RealTimeItemConverter.cs
@@ -0,0 +1,47 @@
+﻿using Pitman.Infrastructure.RealTimeData.Repository;
+using Pitman.Infrastructure.RealTimeData.Sina;
+
+namespace Pitman.Application.MarketData
+{
+    public class RealTimeItemConverter
+    {
+        public static RealTimeItem Convert(SinaRealTimeData data)
+        {
+            RealTimeItem item = new RealTimeItem
+            {
+                Amount = data.Amount,
+                High = data.High,
+                Low = data.Low,
+                Price = data.Price,
+                Time = data.Time,
+                TodayOpen = data.TodayOpen,
+                Volume = data.Volume,
+                YesterdayClose = data.YesterdayClose,
+
+                BuyFivePrice = data.BuyFivePrice,
+                BuyFiveVolume = data.BuyFiveVolume,
+                BuyFourPrice = data.BuyFourPrice,
+                BuyFourVolume = data.BuyFourVolume,
+                BuyThreePrice = data.BuyThreePrice,
+                BuyThreeVolume = data.BuyThreeVolume,
+                BuyTwoPrice = data.BuyTwoPrice,
+                BuyTwoVolume = data.BuyTwoVolume,
+                BuyOnePrice = data.BuyOnePrice,
+                BuyOneVolume = data.BuyOneVolume,
+
+                SellFivePrice = data.SellFivePrice,
+                SellFiveVolume = data.SellFiveVolume,
+                SellFourPrice = data.SellFourPrice,
+                SellFourVolume = data.SellFourVolume,
+                SellThreePrice = data.SellThreePrice,
+                SellThreeVolume = data.SellThreeVolume,
+                SellTwoPrice = data.SellTwoPrice,
+                SellTwoVolume = data.SellTwoVolume,
+                SellOnePrice = data.SellOnePrice,
+                SellOneVolume = data.SellOneVolume,
+            };
+
+            return item;
+        }
+    }
+}
