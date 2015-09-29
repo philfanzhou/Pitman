8960b5534546f416b97578be02001f9cdf1b9baa
philfanzhou <philfanzhou@hotmail.com>
2015-09-29 23:24:06 +0800

    建立东财与Wm数据项目

---
+++ 4_Infrastructure/Pitman.Infrastructure.HistoryData.Eastmoney/EastmoneyDataReader.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Pitman.Infrastructure.HistoryData.Eastmoney
+{
+    public class EastmoneyDataReader
+    {
+        internal static string OrgPercentUrl = "http://data.eastmoney.com/stockcomment/{0}.html";
+
+        internal static string GetDataFromUrl(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Timeout = 5000;
+            request.Proxy = null;
+
+            try
+            {
+                WebResponse response = request.GetResponse();
+
+                try
+                {
+                    Stream stream = response.GetResponseStream();
+                    StreamReader streamReader = new StreamReader(stream, Encoding.GetEncoding("gb2312"));
+                    return streamReader.ReadToEnd();
+                }
+                finally
+                {
+                    if (response != null)
+                    {
+                        response.Close();
+                        response = null;
+                    }
+                }
+            }
+            catch
+            {
+                //访问Url的过程中出现网络异常
+                return null;
+            }
+            finally
+            {
+                if (request != null)
+                {
+                    request.Abort();
+                    request = null;
+                }
+            }
+        }
+    }
+}
