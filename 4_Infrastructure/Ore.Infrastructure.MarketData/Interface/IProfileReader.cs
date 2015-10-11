f7a2fce1b72331fbf0a32abeafe640d20e82d11c
philfanzhou <philfanzhou@hotmail.com>
2015-10-11 13:49:22 +0800

    独立了Ore项目

--- 4_Infrastructure/Ore.Infrastructure.MarketData/Interface/IProfileReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Ore.Infrastructure.MarketData
-{
-    public interface IProfileReader
-    {
-        IEnumerable<ISecurityProfile> GetAll();
-    }
-}
