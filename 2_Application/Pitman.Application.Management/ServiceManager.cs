15c867152ddc5ab394462b2c2312dfe195201070
philfanzhou <philfanzhou@hotmail.com>
2015-10-12 23:03:33 +0800

    添加数据服务管理

---
+++ 2_Application/Pitman.Application.Management/ServiceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Pitman.Application.Management
+{
+    public class ServiceManager
+    {
+        // todo: singleton
+
+        /// <summary>
+        /// 服务控制timer，每30秒刷新一次
+        /// </summary>
+        private readonly Timer ServiceStatusCheckTimer = new Timer(30000);
+
+        private ServiceManager()
+        {
+            ServiceStatusCheckTimer.Elapsed += ServiceStatusCheckTimer_Elapsed;
+            ServiceStatusCheckTimer.Enabled = true;
+        }
+
+        private void ServiceStatusCheckTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime now = DateTime.Now;
+
+            //if(now.Hour)
+        }
+    }
+}
