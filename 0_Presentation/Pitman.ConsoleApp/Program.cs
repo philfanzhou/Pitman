61f4de42f3a5cd60b8b3ce7027a27ce3a981d6af
philfanzhou <philfanzhou@hotmail.com>
2016-01-15 16:08:46 +0800

    调整了console程序和启动逻辑

--- 0_Presentation/Pitman.ConsoleApp/Program.cs
+++ 0_Presentation/Pitman.ConsoleApp/Program.cs
@@ -1,40 +1,78 @@
 ﻿using Pitman.Distributed.WebApi;
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Threading;
+using System.Linq;
 
 namespace Pitman.ConsoleApp
 {
     class Program
     {
-        private static Mutex mutex;
-        
+        private static Mutex mutex = new Mutex(true, "Pitman.OnlyRun");
+
+        private static WebApiServer _webApiServer;
+        private static int _webApiPort = 9999;
+
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Green;
+
+            if (IsAlreadyRunning())
+            {
+                return;
+            }
 
             Console.WriteLine("=================================");
             Console.WriteLine("Pitman.ConsoleApp");
             Console.WriteLine("=================================");
 
-            mutex = new Mutex(true, "Pitman.OnlyRun");
+            try
+            {
+                StarService();
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.ToString());
+            }
+
+            WaitingForExit();
+        }
+
+        private static bool IsAlreadyRunning()
+        {
             if (!mutex.WaitOne(0, false))
             {
-                Console.WriteLine("Pitman.ConsoleApp is already running");
+                Console.WriteLine("Pitman is already running, please don't run the application again.");
                 Console.WriteLine("Press any key to exit....");
                 Console.ReadKey();
+                return true;
             }
             else
             {
-                WebApiServer server = new WebApiServer();
-                server.Open(9999);
+                return false;
+            }
+        }
 
-                while (true)
+        private static void WaitingForExit()
+        {
+            Console.WriteLine("\r\npress exit to close the application....");
+            while (true)
+            {
+                if (Console.ReadLine().ToLower() == "exit")
                 {
-                    if(Console.ReadLine().ToLower() == "exit")
-                    {
-                        break;
-                    }
+                    break;
                 }
+            }
+        }
+
+        private static void StarService()
+        {
+            _webApiServer = new WebApiServer(_webApiPort);
+            if(_webApiServer.Open())
+            {
+                Console.WriteLine(string.Format("Service is listening at {0}", _webApiServer.BasicAddress));
             }
         }
     }
--- 1_Distributed/Pitman.Distributed.WebApi/WebApiServer.cs
+++ 1_Distributed/Pitman.Distributed.WebApi/WebApiServer.cs
@@ -1,15 +1,49 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Web.Http;
 using System.Web.Http.SelfHost;
 
 namespace Pitman.Distributed.WebApi
 {
-    public class WebApiServer
+    public class WebApiServer : IDisposable
     {
+        private readonly int _port;
         private HttpSelfHostServer _httpServer;
 
-        public void Open(int port)
+        public WebApiServer(int port)
         {
-            var configuration = new HttpSelfHostConfiguration(string.Format("http://localhost:{0}", port));
+            _port = port;
+        }
+
+        public void Dispose()
+        {
+            if(_httpServer != null)
+            {
+                _httpServer.Dispose();
+                _httpServer = null;
+            }
+        }
+
+        public string BasicAddress
+        {
+            get
+            {
+                if (_httpServer == null)
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    return (_httpServer.Configuration as HttpSelfHostConfiguration).BaseAddress.ToString();
+                }
+            }
+        }
+
+        public bool Open()
+        {
+            var configuration = new HttpSelfHostConfiguration(string.Format("http://localhost:{0}", _port));
             _httpServer = new HttpSelfHostServer(configuration);
 
             configuration.MapHttpAttributeRoutes();
@@ -33,7 +67,42 @@ namespace Pitman.Distributed.WebApi
                 }
             );
 
-            _httpServer.OpenAsync();
+            _httpServer.OpenAsync().Wait();
+
+            return CheckListeningStatus();
+        }
+
+        /// <summary>
+        /// 获取当前服务的监听状况
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckListeningStatus()
+        {
+            //获取本地计算机的网络连接和通信统计数据的信息
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+
+            //返回本地计算机上的所有Tcp监听程序
+            IPEndPoint[] ipsTCP = ipGlobalProperties.GetActiveTcpListeners();
+
+            //返回本地计算机上的所有UDP监听程序
+            IPEndPoint[] ipsUDP = ipGlobalProperties.GetActiveUdpListeners();
+
+            //返回本地计算机上的Internet协议版本4(IPV4 传输控制协议(TCP)连接的信息。
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+            List<int> allPorts = new List<int>();
+            foreach (IPEndPoint ep in ipsTCP) allPorts.Add(ep.Port);
+            foreach (IPEndPoint ep in ipsUDP) allPorts.Add(ep.Port);
+            foreach (TcpConnectionInformation conn in tcpConnInfoArray) allPorts.Add(conn.LocalEndPoint.Port);
+
+            if(allPorts.Contains(_port))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
